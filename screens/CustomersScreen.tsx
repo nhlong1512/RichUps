544d44902ae3f7febbe241d68c6065a72c16c7fe
nhlong1512 <20521569@gm.uit.edu.vn>
2023-04-06 18:49:51 +0700

    Setup grapql and deployed api/mollified-giraffe to Stepzen

--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,14 +1,36 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
+import { TextInput } from "react-native-paper";
 
 const CustomersScreen = () => {
+  const [searchText, setSearchText] = useState<string>("");
   return (
-    <SafeAreaView>
-      <Text>CustomersScreen</Text>
-    </SafeAreaView> 
-  )
-}
+    <ScrollView className="bg-[#59c1cc]">
+      <Image
+        source={require("../assets/images/customer_banner.jpeg")}
+        className="w-full h-64"
+      />
+      <TextInput
+        mode="outlined"
+        label="Search by Customer"
+        value={searchText}
+        onChangeText={(text) => setSearchText(text)}
+        className="pt-[10px] px-[10px]"
+        outlineColor="transparent"
+        activeOutlineColor="#59c1cc"
+        placeholderTextColor="#969393"
+      />
+    </ScrollView>
+  );
+};
 
-export default CustomersScreen
+export default CustomersScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
